3f03fadd05f796278672cbb15531762c7fa0020b
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2025-02-10 19:31:36 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Money Control</title>
+</head>
+<body>
+    <main>
+        <div class="index-container">
+            <div>
+                <h1>Money Control</h1>
+                <p>The system that help you to control your money!</p>
+            </div>
+            <div>
+                <img class="index-photo" src="images/main-photo.jpeg" alt="Image logo with money and calculator">
+            </div>
+            <div>
+                <span><a href="#">Transactions | </a></span>
+                <span><a href="#">Categories | </a></span>
+                <span><a href="#">Graphics</a></span>
+            </div>
+        </div>
+        <footer>
+            <div class="footer-container">
+                <a href="">Copyright © 2025 <a href="https://github.com/lucasFreitasDev97">Lucas Freitas</a>. All rights reserved.</a>
+            </div>
+        </footer>
+    </main>
+</body>
+</html>
