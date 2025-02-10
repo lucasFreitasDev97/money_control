a7dd3bcec4a838871efb2aeb6c9da2231fd9add9
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2025-02-10 19:54:03 -0300

    working in progress

---
+++ transactions.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Transactions</title>
+</head>
+<body>
+    <main>
+        <div class="index-option-container">
+            <h1>Transactions</h1>
+            <div>
+                <button>+ Add New Register</button>
+            </div>
+            <div>
+                <table class="centered-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Type</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
